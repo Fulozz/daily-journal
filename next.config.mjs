9908f3d8557d5c815e7504358445d4ee9284b636
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2025-03-09 18:04:33 -0300

    Add PWA support and remove cookie management from InstallBanner

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    pwa: {
+        dest: 'public', 
+        register: true, 
+        skipWaiting: true, 
+        clientsClaim: true,
+      },
+};
 
 export default nextConfig;
